bdf05dc61e51362e44dab0291a60363e38736f9a
Gonzalo Copes PROYECTOS <gonza_95_boca@hotmail.es>
2025-05-13 13:56:00 -0300

    nuevos cmabios

---
+++ particulas.js
@@ -0,0 +1,95 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 60,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": { "value": "#00c0ff" },
+    "shape": {
+      "type": "circle",
+      "stroke": { "width": 0, "color": "#000000" },
+      "polygon": { "nb_sides": 5 }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 0.8,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 4,
+        "size_min": 0.3,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#00c0ff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 2,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "bounce",
+      "bounce": true,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"  // "grab" si preferís que se conecten al cursor
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push" // agrega partículas al hacer clic
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 200,
+        "line_linked": { "opacity": 0.6 }
+      },
+      "bubble": {
+        "distance": 250,
+        "size": 6,
+        "duration": 2,
+        "opacity": 0.4
+      },
+      "repulse": {
+        "distance": 100,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
--- script.js
+++ script.js
@@ -2,20 +2,28 @@ let menuVisible = false;
 
 //esto oculta y muestra el menu del portafolio
 
-function mostrarOcultarMenu(){
-    if(menuVisible){
-        document.getElementById("nav").classList ="";
+function mostrarOcultarMenu() {
+    if (menuVisible) {
+        document.getElementById("nav").classList = "";
         menuVisible = false;
-    }else{
-        document.getElementById("nav").classList ="responsive";
+    } else {
+        document.getElementById("nav").classList = "responsive";
         menuVisible = true;
     }
 }
 
 //y esta funcion cuando seleccionas algo del menu se cierra
 
-function seleccionar(){
+function seleccionar() {
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
+
+// LOADER 
+window.addEventListener("load", () => {
+  const loader = document.getElementById("loader");
+  setTimeout(() => {
+    loader.classList.add("hidden");
+  }, 2000); 
+});
 
